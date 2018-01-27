4424d3daacbbc61a6e00b6583f29f16f919cb372
Miroslav Hájek <mirkousko@gmail.com>
2018-01-27 22:17:32 +0100

    Save data after measurement

--- measure.py
+++ measure.py
@@ -1,18 +1,22 @@
+import db
 from datetime import datetime
 from sensors.ds18b20 import Thermometer
 from sensors.bmp280 import BMP280
 
+LOCATION = 'bratislava'
 ALTITUDE = 150
+
+database = db.Weather(LOCATION)
 t = Thermometer()
 p = BMP280()
 
 p.measure()
 date = datetime.now().replace(microsecond=0)
-
 measurement = {
         'dtm': date.isoformat(),
         't': t.temperature,
         'p': p.mslp_pressure(ALTITUDE),
         'p_raw': p.pressure
     }
-print(measurement)
+database.insert(measurement)
+print(database.view())
