115cdbc692c62ce749221e0aa8bed3c72fc4c637
Miroslav Hájek <mirkousko@gmail.com>
2018-02-01 22:23:26 +0100

    Access measurements in database

--- charts.py
+++ charts.py
@@ -9,3 +9,17 @@ class MeteoGraphs:
     def __init__(self, temp_title, press_title):
         self.temp_title = temp_title
         self.press_title = press_title
+
+    def query_interval(self, start, finish):
+        session = db.Session()
+        data = (session
+                .query(db.Weather)
+                .filter((cast(db.Weather.dtm, DateTime) <=
+                         cast(finish, DateTime)) &
+                        (cast(db.Weather.dtm, DateTime) >=
+                         cast(start, DateTime)))
+                .order_by(db.Weather.dtm).all())
+
+        return {'dates': [x.dtm for x in data],
+                't': [y.temperature for y in data]
+                'p': [y.pressure for y in data]}
