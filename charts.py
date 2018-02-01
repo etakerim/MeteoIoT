b17cf12cbb09d0168fadf6267467b338c7f700d7
Miroslav Hájek <mirkousko@gmail.com>
2018-02-01 22:36:07 +0100

    Utility to plot single quantity

--- charts.py
+++ charts.py
@@ -21,5 +21,34 @@ class MeteoGraphs:
                 .order_by(db.Weather.dtm).all())
 
         return {'dates': [x.dtm for x in data],
-                't': [y.temperature for y in data]
+                't': [y.temperature for y in data],
                 'p': [y.pressure for y in data]}
+
+    def plot_quantity(ax, title, x, y, start, finish, color, hourtick):
+        FONT_SIZE = 9
+        WDAY_ABBR = ['Pon', 'Uto', 'Str', 'Štv', 'Pia', 'Sob', 'Ned']
+
+        now = start
+        while now <= finish:
+            ax.axvline(x=now, ls='-', color='#dddddd')
+            now += dt.timedelta(days=1)
+
+        ax.set_xlim(start, finish)
+        ax.xaxis.grid(False)
+        ax.yaxis.grid(True)
+        ax.set_title(title)
+        ax.xaxis.set_major_locator(mdates.HourLocator(interval=hourtick))
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%H'))
+        ax.tick_params(axis='both', which='major', labelsize=FONT_SIZE)
+
+        ax.plot(x, y, '-', color=color)
+
+        y_min, y_max = ax.get_ylim()
+        if y_min < 0:
+            ax.axhline(y=0, color='y')
+
+        now = start
+        while now <= finish:
+            wd = '{} {}'.format(WDAY_ABBR[now.weekday()], now.day)
+            ax.text(now, y_min, wd, fontsize=FONT_SIZE)
+            now += dt.timedelta(days=1)
