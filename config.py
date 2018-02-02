7b0f7c8d41c487cec4d5b1398e4d7476d88e469e
Miroslav Hájek <mirkousko@gmail.com>
2018-02-02 19:56:23 +0100

    Add global project configuration

---
+++ config.py
@@ -0,0 +1,8 @@
+LOCATION_ID = 1
+
+GRAPH_PATHS = [{'name': 'Počasie za posledné 3 dni',
+                'path': 'last-3-days.png'},
+               {'name': 'Počasie za posledných 10 dní',
+                'path': 'last-10-days.png'},
+               {'name': 'Štatistika za celé obdobie',
+                 'path': 'statistics.png'}]
