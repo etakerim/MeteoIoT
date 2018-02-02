65eb57ac398c61008f74b8b3feba382c201414c7
Miroslav Hájek <mirkousko@gmail.com>
2018-02-02 19:56:48 +0100

    Use location from config

--- measure.py
+++ measure.py
@@ -1,19 +1,18 @@
-import os
 import db
+import config
 from sensors.ds18b20 import Thermometer
 from sensors.bmp280 import BMP280
 
-LOCATION = os.environ['LOCATION_ID']
 t = Thermometer()
 p = BMP280()
 
 session = db.Session()
-place = session.query(db.Location).get(LOCATION)
+place = session.query(db.Location).get(config.LOCATION_ID)
 
 p.measure()   # Wake up sensor after inactivity
 p.measure()   # Read actual pressure
 session.add(db.Weather(
-                location_id=LOCATION,
+                location_id=config.LOCATION_ID,
                 temperature=t.temperature,
                 pressure=p.mslp_pressure(place.altitude))
             )
