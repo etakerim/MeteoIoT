bbd68eb75943576a5629314781503d503425ce2f
Miroslav Hájek <mirkousko@gmail.com>
2018-02-03 19:52:04 +0100

    Rewrite to class

--- infodisplay.py
+++ infodisplay.py
@@ -1,48 +1,73 @@
+import os
 import time
 from RPi import GPIO
 from PIL import ImageFont
 from luma.core.interface.serial import i2c
 from luma.oled.device import ssd1306
 from luma.core.render import canvas
+from sensors.bmp280 import BMP280
+from sensors.ds18b20 import Thermometer
 
 
-a_screen = ssd1306(i2c(port=1, address=0x3C))
-b_screen = ssd1306(i2c(port=1, address=0x3D))
-fontsize = 11
-font = ImageFont.truetype('DejaVuSans-Bold.ttf', fontsize)
+class Display:
 
-def off(channel):
-    global a_screen
-    a_screen.hide()
+    def __init__(self):
+        fontsize = 11
+        font = ImageFont.truetype('DejaVuSans-Bold.ttf', fontsize)
 
-def on(channel):
-    global a_screen
-    a_screen.show()
+        self.style = dict(fill='white', font=font)
+        self.line = [(5, 2 * fontsize * i) for i in range(3)]
+        self.disp_on = True
+        self.a_screen = ssd1306(i2c(port=1, address=0x3C))
+        self.b_screen = ssd1306(i2c(port=1, address=0x3D))
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(17, GPIO.FALLING, callback=off, bouncetime=200)
-GPIO.add_event_detect(27, GPIO.FALLING, callback=on, bouncetime=200)
+    def control_setup(self, disp_btn, measure_btn):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(disp_btn, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(measure_btn, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(disp_btn, GPIO.FALLING,
+                              callback=self.turn_onoff, bouncetime=500)
+        GPIO.add_event_detect(measure_btn, GPIO.FALLING,
+                              callback=self.refresh, bouncetime=200)
 
+    def turn_onoff(self, channel):
+        if self.disp_on:
+            self.a_screen.hide()
+            self.b_screen.hide()
+        else:
+            self.a_screen.show()
+            self.b_screen.show()
+        self.disp_on = not self.disp_on
 
-line = [(5, 2 * fontsize * i) for i in range(3)]
-t = 23.345
-p = 1015.45
-interface = 'eth0'
-ip = '192.168.100.100'
-mac = 'b8:23:98:2d:1c:f3'
+    def weather(self, t, p):
+        with canvas(self.a_screen) as draw:
+            draw.text(self.line[0], 'WEATHER', **self.style)
+            draw.text(self.line[1], 't = {:.2f} °C'.format(t), **self.style)
+            draw.text(self.line[2], 'p = {:.2f} hPa'.format(p), **self.style)
 
-style = dict(fill='white', font=font)
-with canvas(a_screen) as draw:
-    draw.text(line[0], 'WEATHER', **style)
-    draw.text(line[1], 't = {:.2f} °C'.format(t), **style)
-    draw.text(line[2], 'p = {:.2f} hPa'.format(p), **style)
+    def localnet(self, ifnet, ip, mac):
+        with canvas(self.b_screen) as draw:
+            draw.text(self.line[0], 'Ethernet - {}'.format(ifnet),
+                      **self.style)
+            draw.text(self.line[1], ip, **self.style)
+            draw.text(self.line[2], mac, **self.style)
 
-with canvas(b_screen) as draw:
-    draw.text(line[0], 'Ethernet - {}'.format(interface), **style)
-    draw.text(line[1], ip, **style)
-    draw.text(line[2], mac, **style)
+    def refresh(self, channel):
+        t = Thermometer()
+        p = BMP280()
+        p.measure()
+
+        h = 150
+        ip = '192.168.100.100'
+        mac = 'b8:23:98:2d:1c:f3'
+        interface = 'eth0'
+
+        self.weather(t.temperature, p.mslp_pressure(h))
+        self.localnet(interface, ip, mac)
+
+display = Display()
+display.control_setup(17, 27)
+display.refresh(None)
 
 while True:
     time.sleep(1)
