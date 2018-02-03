6a0b908c598da8d53667690be5a6a2456cec07d4
Miroslav Hájek <mirkousko@gmail.com>
2018-02-03 20:03:50 +0100

    Savely call refresh in callback event

--- infodisplay.py
+++ infodisplay.py
@@ -1,6 +1,4 @@
-import os
 import signal
-import time
 from RPi import GPIO
 from PIL import ImageFont
 from luma.core.interface.serial import i2c
@@ -8,6 +6,7 @@ from luma.oled.device import ssd1306
 from luma.core.render import canvas
 from sensors.bmp280 import BMP280
 from sensors.ds18b20 import Thermometer
+
 
 class Display:
 
@@ -26,9 +25,11 @@ class Display:
         GPIO.setup(disp_btn, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setup(measure_btn, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.add_event_detect(disp_btn, GPIO.FALLING,
-                              callback=self.turn_onoff, bouncetime=500)
+                              callback=self.turn_onoff,
+                              bouncetime=300)
         GPIO.add_event_detect(measure_btn, GPIO.FALLING,
-                              callback=self.refresh, bouncetime=200)
+                              callback=lambda ch: self.refresh,
+                              bouncetime=200)
 
     def turn_onoff(self, channel):
         if self.disp_on:
@@ -37,6 +38,7 @@ class Display:
         else:
             self.a_screen.show()
             self.b_screen.show()
+            self.refresh()
         self.disp_on = not self.disp_on
 
     def weather(self, t, p):
@@ -52,7 +54,7 @@ class Display:
             draw.text(self.line[1], ip, **self.style)
             draw.text(self.line[2], mac, **self.style)
 
-    def refresh(self, channel):
+    def refresh(self):
         t = Thermometer()
         p = BMP280()
         p.measure()
@@ -67,5 +69,5 @@ class Display:
 
 display = Display()
 display.control_setup(17, 27)
-display.refresh(None)
+display.refresh()
 signal.pause()
